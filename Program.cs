62518d15837143b69849178a8a5ddd9533719e9b
José Romualdo <joseromualdo@outlook.com>
2025-09-08 22:40:09 +0000

    adiciona arquivos iniciais do projeto e implementa a classe Televisao

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao(55f);
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
+
+tv.Volume = -35;
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.Volume = 120;
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
---
+++ Televisao.cs
@@ -0,0 +1,22 @@
+public class Televisao
+{
+    private const int VOL_MAX = 100;
+    public Televisao(float tamanho)
+    {
+        Tamanho = tamanho;
+    }
+
+    public float Tamanho { get; }
+    public int Resolucao { get; set; }
+    public int Volume { get; private set; }
+    public int Canal { get; set; }
+    public bool Estado { get; set; }
+
+    public void AumentarVolume()
+    {
+        if(Volume < VOL_MAX)
+            Volume++;
+        else
+            Console.WriteLine("TV já está no max.");
+    }
+}
